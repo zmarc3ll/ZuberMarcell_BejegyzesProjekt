5826a1cd59c4886f801649ee9f5d6ae93d6e962d
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-09-29 08:47:35 +0200

    Bejegyzes class get + set beallítása értékek felvétele

--- src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
+++ src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
@@ -1,5 +1,45 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
 
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        this.szerkesztve = LocalDateTime.now();
+    }
 }
