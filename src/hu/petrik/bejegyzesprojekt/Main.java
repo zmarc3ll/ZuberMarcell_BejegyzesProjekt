da7afbffa2771eca63e37a772ba1b6dd8b4a9a35
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-09-29 09:27:18 +0200

    listák felvétele a Mian- ben + szám bekérése

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,7 +1,21 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static List<Bejegyzes> bejegyzesek1 = new ArrayList<>();
+    private static List<Bejegyzes> bejegyzesek2 = new ArrayList<>();
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjon meg egy darabszámot! ");
+        int darabszam = sc.nextInt();
+        while (darabszam < 0) {
+            System.out.println("Adjon meg egy természetes számot!");
+            darabszam = sc.nextInt();
+        }
     }
 }
