859c873e8ba509b376d2dac32dca69f50c2ce57e
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-09-29 10:06:03 +0200

    bejegyzés hozzáadása

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,6 +1,7 @@
 package hu.petrik.bejegyzesprojekt;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,10 +13,28 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Adjon meg egy darabszámot! ");
-        int darabszam = sc.nextInt();
+        int darabszam=sc.nextInt();
+        /*try {
+            darabszam = sc.nextInt();
+            System.out.println("Adjon meg egy természetes számot!");
+            darabszam = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Adjon meg egy természetes számot!");
+        } */
         while (darabszam < 0) {
             System.out.println("Adjon meg egy természetes számot!");
             darabszam = sc.nextInt();
+        }
+        Bejegyzes b;
+        String szerzo;
+        String tartalom;
+        for (int i = 0; i < darabszam; i++) {
+            System.out.println("Adjon meg egy szerzőt: ");
+            szerzo=sc.nextLine();
+            System.out.println("Adja meg a bejegyzése tartalmát: ");
+            tartalom=sc.nextLine();
+            b=new Bejegyzes(szerzo,tartalom);
+            bejegyzesek1.add(b);
         }
     }
 }
